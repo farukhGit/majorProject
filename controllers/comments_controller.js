cf9f0833b6b66682231f4af0bf444a86490cf094
farukhGit <58646969+farukhGit@users.noreply.github.com>
2020-01-24 13:13:00 +0530

    try catch actions and convert to async await

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -2,42 +2,42 @@ const Comment = require('../models/comments_schema');
 const Post = require('../models/posts_schema');
 
 module.exports.create = (req, res)=>{
-   Post.findById(req.body.post, function(err, post){
-       
-        if(post){
-            Comment.create({
-                content : req.body.content,
-                user : req.user._id,
-                post : req.body.post
-            }, function(err, comment){
-                if(err){
-                    console.log('Error creating comment.');
-                    return;
-                }
-
-                post.comments.push(comment);
-                post.save();
-
-                return res.redirect('back');
-            }); 
-        }
-   });
+       try {
+        Post.findById(req.body.post, function(err, post){  
+            if(post){
+                Comment.create({
+                    content : req.body.content,
+                    user : req.user._id,
+                    post : req.body.post
+                }, function(err, comment){
+                    post.comments.push(comment);
+                    post.save();
+                    return res.redirect('back');
+                }); 
+            }
+       });
+       } catch (error) {
+           console.log('Error : ', err);
+           return;
+       }
 }
 
 module.exports.deleteComment = (req, res)=>{
-    Comment.findById(req.params.id, function(err, comment){
-        if(err){
-            console.log('Error deleting the comment.');
-            return;
-        }
-        if(req.user.id == comment.user){
-            let postId = comment.post;
-            comment.remove();
-            Post.findByIdAndUpdate(postId, {$pull : {comments : req.params.id}}, function(err, post){
-            return res.redirect('back');
+    try {
+        Comment.findById(req.params.id, function(err, comment){
+            
+            if(req.user.id == comment.user){
+                let postId = comment.post;
+                comment.remove();
+                Post.findByIdAndUpdate(postId, {$pull : {comments : req.params.id}}, function(err, post){
+                return res.redirect('back');
+            });
+            }else{
+                return res.redirect('back');
+            }
         });
-        }else{
-            return res.redirect('back');
-        }
-    });
+    } catch (error) {
+        console.log('Error : ', err);
+        return;
+    }
 }
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,34 +1,35 @@
 const Post = require('../models/posts_schema');
 const Comment = require('../models/comments_schema');
+
 module.exports.create = function(req, res){
-    Post.create({
-        content : req.body.content,
-        user : req.user._id
-    }, function(err, post){
-        if(err){
-            console.log('Error creating the post.');
-            return;
-        }
-        return res.redirect('back');
-    })
+    try {
+        Post.create({
+            content : req.body.content,
+            user : req.user._id
+        }, function(err, post){
+            
+            return res.redirect('back');
+        });
+    } catch (error) {
+        console.log('Error : ', err);
+        return;
+    }   
 }
 
-module.exports.destroy = (req, res)=>{
-    Post.findById(req.params.id, function(err, post){
-        if(err){
-            throw err;
-        }
+module.exports.destroy = async (req, res)=>{
+    try {
+        let post = await Post.findById(req.params.id); 
+        
         if(req.user.id == post.user){
             post.remove();
-            Comment.deleteMany({post : req.params.id}, function(err){
-                if(err){
-                    console.log('Error deleting the posts.');
-                    return;
-                }
-                return res.redirect('back');
-            })
-        }else{
+            Comment.deleteMany({post : req.params.id}); 
             return res.redirect('back');
+        }else{
+            return res.redirect('user_home');
         }
-    })
+
+    } catch (error) {
+        console.log('Error : ', err);
+        return;
+    }
 }
